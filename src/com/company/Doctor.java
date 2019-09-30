230115c2cd6d04240904774ee7fdbb396b428d5a
csalley95 <c1salley@student.bridgew.edu>
2019-09-30 15:05:49 -0400

    Intro to Inheritance

---
+++ src/com/company/Doctor.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Doctor {
+    protected String AlmaMater;
+    private double fee;
+
+    public void treatPatient(Person patient){
+        //do something here Wednesday
+    }
+}
